5f4699a38cab9310a56a2afff55e55932cf56be4
mbany786 <m.anayetullah@gmail.com>
2026-04-08 00:02:54 -0400

    Update streamlit_app.py
    added snowflake connector for troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import snowflake.connector
 
-session = get_active_session()
+# Replace get_active_session() with this:
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-st.title('🥤 Customize Your Smoothie! 🥤')
+st.title('🍓 Customize Your Smoothie! 🍓')
 st.write('Choose the fruits you want in your custom Smoothie!')
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options")
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 cn_dataframe = my_dataframe.select(col("FRUIT_NAME")).to_pandas()
 
@@ -21,19 +23,15 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-
         st.success('Your Smoothie is ordered!', icon="✅")
